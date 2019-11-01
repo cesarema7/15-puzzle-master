ecb71ea845e6c2cf90c30a9b26fa64feca135316
cesarema7 <36248828+cesarema7@users.noreply.github.com>
2019-11-01 15:33:26 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### PRIMER PROTOTIPO 
+	Este primer prototipo, está conformado por un GRID de proporciones 3x3 
+	con un color de fondo Naranja y un      <h1> PRIMER PROTOTIPO </h1>
---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import levelFactory from './../lib/levels-factory';
+import Game from './Game';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const level = props.level ? props.level : levelFactory(3 ** 2);
+    const originalLevel = Object.assign({}, level);
+
+    this.state = {
+      original: originalLevel,
+      level,
+    };
+  }
+
+ 
+
+  render() {
+    const { className } = this.props;
+    return (      
+      <div className={className}>    
+      <h1>Primer Prototipo</h1>    
+        <Game
+          gridSize={3}
+          tileSize={90}
+          numbers={this.state.level.tileSet}
+          onResetClick={this.onResetClick}
+          onNewClick={this.onNewClick}
+          original={this.state.original.tileSet}
+        />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  level: PropTypes.shape({
+    tileSet: PropTypes.arrayOf(PropTypes.number).isRequired,
+  }),
+};
+
+export default styled(App)`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
